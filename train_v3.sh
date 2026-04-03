1dc8d396abbf2f7080678ab400ff6108d78b41ec
Edward He <80665238+EDHE08232001@users.noreply.github.com>
2026-04-03 14:24:04 -0400

    Change training script from go_one_episode to go_one_training_run

--- train_v3.sh
+++ train_v3.sh
@@ -23,4 +23,4 @@ source $HOME/PokemonRL/v3env/local/bin/activate
 bash $HOME/PokemonRL/pre_run_v3.sh
 
 # ── Run the V3 training script loop ──────────────────────────────────────────
-bash src/v3/go_one_episode.sh
+bash src/v3/go_one_training_run.sh
